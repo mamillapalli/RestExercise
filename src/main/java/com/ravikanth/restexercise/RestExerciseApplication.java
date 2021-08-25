17e8dad03f6828eed8689f03ca91216f6a9f915c
Ravi Kant <ravikant@chinasystems-me.com>
2021-08-25 11:32:13 +0400

    Initial commit

---
+++ src/main/java/com/ravikanth/restexercise/RestExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.ravikanth.restexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ravikanth/restexercise/RestExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravikanth.restexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
